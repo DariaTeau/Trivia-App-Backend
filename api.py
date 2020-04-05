161ebeaaac710635e3dbc4938f634723d99edab4
Daria Teau <daria.e.24@gmail.com>
2020-04-05 22:21:34 +0300

    modified so it can be deployed to App Engine

--- api.py
+++ api.py
@@ -1,14 +1,23 @@
 from flask import Flask, request, jsonify
 from urllib.parse import parse_qs
 import pymysql
+import json
+import sqlalchemy
 import os
 
-connection = pymysql.connect(host = 'localhost',
-							user = 'root',
-							password = os.environ['DBPASS'],
-							db = 'trivia',
-							cursorclass = pymysql.cursors.DictCursor)
-
+db = sqlalchemy.create_engine(
+    # Equivalent URL:
+    # mysql+pymysql://<db_user>:<db_pass>@/<db_name>?unix_socket=/cloudsql/<cloud_sql_instance_name>
+    sqlalchemy.engine.url.URL(
+        drivername="mysql+pymysql",
+        username="root",
+        password="ip2020",
+        database="trivia",
+        query={"unix_socket": "/cloudsql/{}".format("firsttry-272817:europe-west3:first-instance")},
+    ),
+    # ... Specify additional properties here.
+    # ...
+)
 app = Flask(__name__)
 
 # root
@@ -22,52 +31,42 @@ def index():
 
 # GET
 #folosit pt verificarea autentificarii
-@app.route('/users/<user>', methods=['GET'])
-def has_user(user):
-	json = parse_qs(request.get_data().decode('utf-8'))
-	for k in json:
-		if len(json[k]) == 1:
-			json[k] = json[k][0]
-	print(json)
-	if len(json["username"]) == 0:
-		return "Invalid input"
-	with connection.cursor() as cursor:
-		sql = "SELECT * FROM `Users` WHERE `username` = '{}'".format(json['username'])
+@app.route('/api/has_user', methods=['GET'])
+def has_user():
+	username = request.args.get("username")
+	with db.connect() as conn:
+		sql = "SELECT * FROM `Users` WHERE `username` = '{}'".format(username)
 		#sql = "SELECT * FROM `Users` WHERE `username` = '{}'".format(user)
-		cursor.execute(sql)
-		result = cursor.fetchone()
+		result = conn.execute(sql).fetchall()
 		if result:
-			return "%s exists" % json['username']
+			return "%s exists" % username
 			#return "%s already exists" % user
-	return "%s not found" % json["username"] 
+	return "%s not found" % username 
 	#return "%s not found" % user
 
 #POST
 #folosit pt creare cont
 @app.route('/api/create_account', methods=['POST'])
 def create_account():
-	json = parse_qs(request.get_data().decode('utf-8'))
-	for k in json:
-		if len(json[k]) == 1:
-			json[k] = json[k][0]
-	print(json)
-	if len(json["username"]) == 0:
+	s = (request.get_data().decode('utf-8'))
+	j = json.loads(s)
+	print(j)
+	if len(j["username"]) == 0:
 		return "Invalid input"
 
-	with connection.cursor() as cursor:
-		sql = "SELECT * FROM `Users` WHERE `username` = '{}'".format(json['username'])
-		cursor.execute(sql)
-		result = cursor.fetchone()
+	with db.connect() as conn:
+		sql = "SELECT * FROM `Users` WHERE `username` = '{}'".format(j['username'])
+		result = conn.execute(sql).fetchall()
 		if result:
-			return "%s already exists" % json['username']
+			return "{} already exists".format(j['username'])
 		
-		sql = "INSERT INTO `Users` (`username`, `points`) VALUES ('{}', '{}')". format(json["username"], json["points"])
-		cursor.execute(sql)
+		sql = "INSERT INTO `Users` (`username`, `points`) VALUES ('{}', '{}')". format(j["username"], j["points"])
+		conn.execute(sql)
 
-	connection.commit()
+	#db.commit()
 
-	return "Insertion for user %s succeded" % json["username"]
+	return "Insertion for user {} succeded".format(j["username"])
 
 # running web app in local machine
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=5000)
+	app.run(host='127.0.0.1', port=8080)
